2a9b052b1a9f2f299b160d287969e75d8a8c7adc
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2026-01-09 11:55:04 +0000

    memory lines

--- LinMazeSolverBaseCode/MazeSolver.cc
+++ LinMazeSolverBaseCode/MazeSolver.cc
@@ -173,12 +173,13 @@ void MazeSolver::uTurn() {
 //displays the path of decisions on screen
 //**********************
 void MazeSolver::displayOnScreen() {
-  display.clear();
+  display.gotoXY(0, 0);
   for (int i = 0; i <= 7; i++) {
     display.print(path[i]);
   }
+
+  display.gotoXY(0, 1);
   for (int j = 8; j <= 15; j++) {
-    display.gotoXY(0, 1);
     display.print(path[j]);
   }
 }
