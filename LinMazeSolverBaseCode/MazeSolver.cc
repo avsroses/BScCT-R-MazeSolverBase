ab72533ee4f6d95d3b2573eaa6124d5cd3c918a4
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2026-01-09 09:35:24 +0000

    update delay

--- LinMazeSolverBaseCode/MazeSolver.cc
+++ LinMazeSolverBaseCode/MazeSolver.cc
@@ -94,7 +94,7 @@ void MazeSolver::identifyJunction() {
 
   if (lineSensorValues[2] > 750) {
     motors.setSpeeds(baseSpeed, baseSpeed);
-    delay(250);
+    delay(100);
     state = LINE_FOLLOWER;
     return;
   }
